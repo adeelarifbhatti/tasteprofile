029625356b48b367613f89110b35184b32b7b5e1
Adeel Arif Bhatti <adeelarifbhatti@gmail.com>
2019-09-17 15:25:33 +0200

    Trying code for getTimesPlayed with tiny version of train_triples_1.txt

--- Server/Servant.java
+++ Server/Servant.java
@@ -1,5 +1,62 @@
 package Server;
+import java.io.FileInputStream;
 
-public class Servant {
+import java.util.Scanner;
+
+import TasteProfile.ProfilerPOA;
+import TasteProfile.UserProfile;
+
+public class Servant extends ProfilerPOA {
+	
+	FileInputStream inputStream;
+
+	@Override
+	public int getTimesPlayed(String song_id) { 
+		try {
+			inputStream = new FileInputStream("train_triplets_test.txt");
+		} catch (Exception e) {
+			System.err.println("Exception: " + e.getMessage());
+			e.printStackTrace(System.out);
+		}
+		Scanner sc = new Scanner(inputStream, "UTF-8");
+		int totalCount= 0;
+		while (sc.hasNextLine()) {
+			String line = sc.nextLine();
+			String[] parts = line.split("\t");
+			if(song_id == parts[0]){
+				Integer i = Integer.valueOf(parts[2]);
+				totalCount=totalCount+i;
+				System.out.println("totalCount is " + totalCount);
+			}
+		sc.close();
+	}
+		
+		return totalCount;
+		
+	}
+
+	@Override
+	public UserProfile getUserProfile(String user_id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getTimesPlayedByUser(String user_id, String song_id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getTopThreeUsersBySong(String song_id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getTopThreeSongsByUser(String song_id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
