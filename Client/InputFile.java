def7c235b89902d4699111f3f932e94af63c0f71
Adeel Arif Bhatti <adeelarifbhatti@gmail.com>
2019-09-17 14:11:17 +0200

    Added the package for Server with classes Servant.java and Server.java. Also modified the code in Client.java

--- Client/InputFile.java
+++ Client/InputFile.java
@@ -3,12 +3,14 @@ package Client;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import TasteProfile.Profiler;
 
 public class InputFile {
 	private Scanner sc;
 	public String method;
 	public String argument1;
 	public String argument2;
+	Profiler profile;
 	public void fileRead() throws FileNotFoundException{
 		
 			FileInputStream inputStream = new FileInputStream("input.txt");
@@ -19,10 +21,20 @@ public class InputFile {
 				++lineNumber;
 				String[] parts = line.split("\t");
 				String method = parts[0];
-				String argument1 = parts[1];
-			    String argument2 = parts.length > 2 ? parts[2] : null;
+				argument1 = parts[1];
+			    argument2 = parts.length > 2 ? parts[2] : null;
 			   
 			    System.out.println(lineNumber + " Method is "+ method + " With  Argument  " + argument1 + " 2nd Argument is " + argument2);
+			    if (method=="getTimesPlayedByUser")
+			    	profile.getTimesPlayedByUser(argument1,argument2);
+			    else if (method=="getTimesPlayed")
+			    	profile.getTimesPlayed(argument1);
+			    else if (method=="getTopThreeUsersBySong")
+			    	profile.getTopThreeUsersBySong(argument1);
+			   
+			    	
+			    		
+			    }
 				
 			}
 		
