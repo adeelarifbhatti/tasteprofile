1892ce834c540f31e263cb5a088fdf5fa5446eab
Adeel Arif Bhatti <adeelarifbhatti@gmail.com>
2019-09-25 15:25:43 +0200

    Fixed a bug in the UserProfileImpl.java

--- src/Implementation/UserProfileImpl.java
+++ src/Implementation/UserProfileImpl.java
@@ -13,10 +13,10 @@ public class UserProfileImpl extends UserProfile{
 		
 	}
 	public UserProfileImpl(String user_id, int total_play_count, TasteProfile.SongCounter songs[], TasteProfile.TopThreeSongs top_three_songs ) {
-		user_id = this.user_id;
-		total_play_count = this.total_play_count;
-		songs = this.songs;
-		top_three_songs = this.top_three_songs;
+		this.user_id = user_id;
+		this.total_play_count = total_play_count;
+		this.songs = songs;
+		this.top_three_songs = top_three_songs;
 		
 	}
 	public UserProfileImpl(String user_id, TasteProfile.TopThreeSongs top_three_songs) {
@@ -34,6 +34,15 @@ public class UserProfileImpl extends UserProfile{
 		this.songs = songs;
 		
 	}
+	public UserProfileImpl(String user_id,int total_play_count, TasteProfile.SongCounter songs[]) {
+
+
+		this.songs=songs;
+		this.total_play_count = total_play_count;
+		
+	}
+	
+	
 
 
 
