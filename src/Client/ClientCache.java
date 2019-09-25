2b327e22ee0dceb22f98be18ac2c813e77d61393
Adeel Arif Bhatti <adeelarifbhatti@gmail.com>
2019-09-25 10:41:35 +0200

    Client side caching, it should work

---
+++ src/Client/ClientCache.java
@@ -0,0 +1,42 @@
+package Client;
+
+import TasteProfile.UserProfile;
+import TasteProfile.Profiler;
+import TasteProfile.SongCounter;
+import TasteProfile.TopThreeSongs;
+import Implementation.UserProfileImpl;
+
+import java.util.HashMap;
+
+
+
+public class ClientCache {
+	Profiler profile;
+	HashMap<String,UserProfile> clientCache = new HashMap<String,UserProfile>();
+	
+	/*  String user_id = null;
+	  int total_play_count ;
+	  SongCounter songs[] = null;
+	  TopThreeSongs top_three_songs = null; */
+	  UserProfile userProfile;
+	  
+		public boolean checkLocalCache(String user_id) {
+			if(clientCache.containsKey(user_id))
+			return true;
+			else 
+			return false;
+		}
+	  
+	public void setUserPrfServer(String user_id) {
+		profile.getUserProfile(user_id);
+		if(profile.getUserProfile(user_id)!=null){
+			UserProfileImpl userPrfl = new UserProfileImpl(user_id,clientCache.get(user_id).total_play_count,clientCache.get(user_id).songs,clientCache.get(user_id).top_three_songs);
+			clientCache.put(user_id, userPrfl);
+			
+		}
+	}
+	
+
+
+
+} //End of Class
--- src/Client/OutputFile.java
+++ src/Client/OutputFile.java
@@ -30,11 +30,11 @@ public class OutputFile {
 	public static void Writer_UserPlayed(String method, String argument1, String argument2, int result, long totalTime) {
 		
 		try {
-			if (result !=0){
+			//if (result !=0){
 			Writer writer = new FileWriter("output.txt",true);
 			writer.write("Song " + argument1 + " Played  " + result + " times " +" By User "+ argument2  +" "+ totalTime +"ms\n" );
 			writer.close();
-			}
+			//}
 		}
 
 		catch(Exception e) {
@@ -47,12 +47,12 @@ public static void outputWriter_topthreeusers(String method, String argument1, T
 		
 	try {
 
-		if (topThreeUsers.topThreeUsers.length>0){
-		Writer writer = new FileWriter("TOPTHRRUSERS.txt",true);
+		//if (topThreeUsers.topThreeUsers.length>0){
+		Writer writer = new FileWriter("topusers.txt",true);
 		for (int i =0; i<topThreeUsers.topThreeUsers.length; i++)
-		writer.write("TOPTHREEUSERS for Song" + argument1 + " are Users  " + topThreeUsers.topThreeUsers[i].user_id + " times played " +topThreeUsers.topThreeUsers[i].songid_play_time+" By User "+ totalTime +"ms\n" );
+		writer.write("Top users for Song " + argument1 + " are " + topThreeUsers.topThreeUsers[i].user_id + " times played " +topThreeUsers.topThreeUsers[i].songid_play_time+" By User "+ totalTime +"ms\n" );
 		writer.close();
-		}
+		//}
 	}
 
 	catch(Exception e) {
@@ -69,12 +69,12 @@ public static void outputWriter_topthreesongs(String method, String argument1, T
 	
 	try {
 
-		if (topThreeSongs.topThreeSongs.length>0){
-		Writer writer = new FileWriter("TOPTHREESongs.txt",true);
+		//if (topThreeSongs.topThreeSongs.length>0){
+		Writer writer = new FileWriter("topsongs.txt",true);
 		for (int i =0; i<topThreeSongs.topThreeSongs.length; i++)
-		writer.write("TOPTHREESongs for User" + argument1 + " are Songs  " + topThreeSongs.topThreeSongs[i].song_id + " times played " +topThreeSongs.topThreeSongs[i].songid_play_time+" By User "+ totalTime +"ms\n" );
+		writer.write("Top Songs " + argument1 + " are " + topThreeSongs.topThreeSongs[i].song_id + " times played " +topThreeSongs.topThreeSongs[i].songid_play_time+" By User "+ totalTime +"ms\n" );
 		writer.close();
-		}
+		//}
 	}
 
 	catch(Exception e) {
