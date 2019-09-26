c4d544af823450cefc86c3b0ed5ee5f9786d32b4
Adeel Arif Bhatti <adeelarifbhatti@gmail.com>
2019-09-26 12:02:41 +0200

    my test file

--- src/Server/Test.java
+++ src/Server/Test.java
@@ -1,44 +1,125 @@
 package Server;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
+import Implementation.SongCounterImpl;
+import Implementation.TopThreeUsersImpl;
+import Implementation.UserCounterImpl;
+import TasteProfile.UserProfile;
 
 //import TasteProfile.ProfilerPOA;
 
 public class Test {
-	int totalCount= 0;
-	String song_id;
+	
 		FileInputStream inputStream;
-		public int getTimesPlayed(String song_id) { 
-			this.song_id=song_id;
+
+		// SongCounterImpl[] song= {songCounter.get(0)};
+		
+		public void createUserID() {
+			List<String>  col0 = new ArrayList<String>();
+			List<String>  col = new ArrayList<String>();
+			List<String> listWithoutDuplicates;
 			try {
-				inputStream = new FileInputStream("train_triplets_test.txt");
-			} catch (Exception e) {
-				System.err.println("Exception: " + e.getMessage());
-				e.printStackTrace(System.out);
-			}
-			Scanner sc = new Scanner(inputStream, "UTF-8");
+				Scanner sc = new Scanner(new File("train_triplets_test.txt"));
+				while (sc.hasNext()) {
+					String line = sc.nextLine();
+					String[] parts = line.split("\t");
+					String myUser_id=parts[1];
 			
-			while (sc.hasNextLine()) {
-				String line = sc.nextLine();
-				String[] parts = line.split("\t");
-				if(song_id == song_id){
-					Integer i = Integer.valueOf(parts[2]);
-					totalCount=totalCount+i;
-					//System.out.println("totalCount is " + totalCount);
+					Scanner sc3 = new Scanner(new File("train_triplets_test.txt"));
+					while (sc3.hasNext()) {
+						String line2 = sc3.nextLine();
+						String[] parts2 = line2.split("\t");
+						String user= parts2[1];
+						if(myUser_id.equals(user)) {
+
+							col0.add(myUser_id);
+							System.out.println("From col0 "+col0);
+
+						}
+					}
+					sc3.close();
+				
 				}
+				sc.close();
+				
+					listWithoutDuplicates = col0.stream()
+							.collect(Collectors.toSet())
+							.stream()
+							.collect(Collectors.toList());
+					Writer writer = new FileWriter("users.txt",true);
+
+					writer.write(listWithoutDuplicates+"  \n");
+					writer.close();
+			}
 			
+					catch (FileNotFoundException e) {
+						System.out.println(new File(".").getAbsolutePath());
+						System.out.println("no file");
+						e.printStackTrace();
+						}
+					catch(Exception e) {
+						System.err.println("Error:" + e.getMessage());
+						e.printStackTrace(System.out);
+					}
 		}
-			sc.close();
-			System.out.println(totalCount);
-			return totalCount;
+
+	
+		public void createProfiles() {
+			SongCounterImpl songCount;
+			ArrayList <UserProfile> userPrfl = new ArrayList<UserProfile>(10);
+			ArrayList <SongCounterImpl> songList = new ArrayList<SongCounterImpl>();
 			
+				/*	Scanner sc2 = new Scanner(new File("train_triplets_test.txt"));
+					while (sc2.hasNext()) {
+						String line2 = sc2.nextLine();
+						String[] parts2 = line2.split("\t");
+						String user= parts2[1];
+
+							if(myUser_id.equals(user)) {
+								String song_id=parts2[0];
+								songCount= new SongCounterImpl(song_id, totalCount);
+								songList.add(songCount);
+								SongCounterImpl[] song= {songList.get(0)};
+								totalCount=totalCount+Integer.parseInt(parts2[2]);
+					
+		
+								System.out.println(myUser_id+ " EQUALS  "+ user+" Song Array "+ song +" TotalPlayTime "+ totalCount);
+								serverPause();
+							}
+							/*else
+								System.out.println(myUser_id+ " NOT EQUALS  "+ user);					
+						}
+					sc2.close(); */
+
+				
 		}
+			
+
 		public static void main (String [] args){
 			Test test= new Test();
-			test.getTimesPlayed("SOAAADD12AB018A9DD");
+			test.createProfiles();
+			test.createUserID();
 			
 			
+		}
+		private void serverPause() {
+			try {
+				Thread.sleep(200);
+			} catch (InterruptedException e) {
+				System.err.println("InterruptedException: " + e.getMessage());
+				e.printStackTrace(System.out);
+			}
 		}
 
 }
